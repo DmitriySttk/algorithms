a00aa2553831e094649284119a920be505efcbd7
faceroll <set.frnx@gmail.com>
2021-12-28 17:09:28 +0300

    init

--- binary_search.rb
+++ binary_search.rb
@@ -1,17 +1,19 @@
 def binary_search(list, item)
   low = 0
   high = list.length - 1
-
+  attempt = 0
   while low <= high
     mid = (low + high) / 2
     guess = list[mid]
 
     if guess == item
-      return mid
+      return attempt
     elsif guess > item
       high = mid - 1
+      attempt += 1
     else
       low = mid + 1
+      attempt += 1
     end
   end
 
@@ -19,7 +21,7 @@ def binary_search(list, item)
 end
 
 my_list = [1, 3, 5, 7, 9, 10, 11, 12, 13]
-puts binary_search(my_list, 13) # => 1
+puts binary_search(my_list, 5) # => 1
 
 # We need to use .inspect here because just printing nil
 # would output an empty string
